994e6ede9ebe4fefb7c5036468937539fc3f9829
marko <marko@PC>
2025-09-25 21:33:51 +0200

    Add project files.

---
+++ Console_2/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Console_2
+{
+    internal class Program
+    {
+        static List<string>list=new List<string>();
+        static void Main(string[] args)
+        {
+            Managemenu();
+            int num=Getnumber(1,6);
+            Console.WriteLine("Sikeres szám megadás"+num);
+            SwitchMenu(num);
+            Console.ReadKey();
+        }
+        static void Managemenu()
+        {
+            Writemenu();
+           
+        }
+        static void Writemenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. adatok listázása");
+            Console.WriteLine("2. adat létrehozása");
+            Console.WriteLine("3. legnagyobb adat");
+            Console.WriteLine("4. legkisebb adat");
+            Console.WriteLine("5. adat törlése");
+            Console.WriteLine("6. kilépés");
+        }
+        static int Getnumber(int min=int.MinValue,int max = int.MaxValue)
+        {
+            Console.WriteLine("Add meg a számot!");
+            string line=Console.ReadLine().Trim(' ',',','.');
+            int resault = 0;
+            if (int.TryParse(line,out resault))
+                if (resault<=max&& resault>=min)
+                {
+                    return resault;
+                }
+                else
+                {
+                    Console.WriteLine("A szám nem a megadott határértéken belül van");
+                }
+            else
+                Console.WriteLine("Nem szám lett megadva");
+
+
+            resault = Getnumber(min, max);
+            return resault;
+        }
+        static void SwitchMenu(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    functionOne();
+                    break;
+                case 2:
+                    functionTwo();
+                    break;
+                case 3:
+                    functionThree();
+                    break;
+                case 4:
+                    functionFour();
+                    break;
+                case 5:
+                    functionFive();
+                    break;
+                case 6:
+                    functionSix();
+                    break;
+                default:
+                    Console.WriteLine("Hibás menupont kerult megadásra");
+                    break;
+            }
+        }
+        static void functionOne()
+        {
+            if (list.Count==0)
+            {
+                Console.WriteLine("Üres a lista");
+            }
+            else
+            {
+                Console.WriteLine("A lista elemei: "+list);
+
+            }
+        }
+        static void functionTwo()
+        {
+            Console.WriteLine("Adj meg egy adatot: ");
+            string addlist = Console.ReadLine().Trim();
+            list.Add(addlist);
+            
+        }
+        static void functionThree()
+        {
+
+        }
+        static void functionFour()
+        {
+
+        }
+        static void functionFive()
+        {
+
+        }
+        static void functionSix()
+        {
+
+        }
+    }
+}
---
+++ Console_2/Serverconnection.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Console_2
+{
+    internal class Serverconnection
+    {
+        HttpClient client = new HttpClient();
+        string baseUrl = "";
+        public Serverconnection(string url)
+        {
+
+            if (!url.StartsWith("http://"))
+            {
+                throw new ArgumentException("Hibás url,http:// meagasása sikertelen");
+
+            }
+            baseUrl = url;
+            GetSubjects();
+        }
+
+        public async Task<List<Subject>> GetSubjects()
+        {
+            List<Subject> resault = new List<Subject>();
+            string url = baseUrl + "/subject";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                resault = JsonSerializer.Deserialize<List<Subject>>(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            return resault;
+
+        }
+        public async Task<List<Users>> GetUsers()
+        {
+            List<Users> resault = new List<Users>();
+            string url = baseUrl + "/user";
+            try
+            {
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                resault = JsonSerializer.Deserialize<List<Users>>(await response.Content.ReadAsStringAsync());
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            return resault;
+
+        }
+        public async Task<Message> PostUser(string username,string password)
+        {
+            Message message = new Message();
+            string url = baseUrl + "/user";
+            try
+            {
+                var jsondata = new
+                {
+                    username = username,
+                    password = password
+                };
+                string jsonString=JsonSerializer.Serialize(jsondata);
+                HttpContent content=new StringContent(jsonString);
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                message = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return message;
+
+        }
+        public async Task<Message> PostSubject( string name)
+        {
+            Message message = new Message();
+            string url = baseUrl + "/subject";
+            try
+            {
+                var jsondata = new
+                {
+                    name = name
+                };
+                string jsonString = JsonSerializer.Serialize(jsondata);
+                HttpContent content = new StringContent(jsonString);
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                message = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return message;
+        }
+        public async Task<Message> DeleteSubject(int id)
+        {
+            Message message = new Message();
+            string url = baseUrl + "/subject/"+id;
+            try
+            {
+                HttpResponseMessage response = await client.DeleteAsync(url);
+                response.EnsureSuccessStatusCode();
+                
+                message = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return message;
+        }
+        public async Task<Subject?> GetbiggestAsync()
+        {
+            string url = baseUrl + "/subject";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var jsonString= await response.Content.ReadAsStringAsync();
+                List<Subject>? subjects = JsonSerializer.Deserialize<List<Subject>>(jsonString);
+                Subject biggest = subjects.OrderByDescending(s => s.name.Count()).First();
+                return biggest;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+
+        }
+        public async Task<Subject?> GetSmallestAsync()
+        {
+            string url = baseUrl + "/subject";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var jsonString = await response.Content.ReadAsStringAsync();
+                List<Subject>? subjects = JsonSerializer.Deserialize<List<Subject>>(jsonString);
+                Subject biggest = subjects.OrderByDescending(s => s.name.Count()).Last();
+                return biggest;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+
+        }
+
+
+    }
+}
